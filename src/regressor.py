Implemented normalization and fixed core mathematical issues. Task 1 appears functional.

src/regressor.py
@@ -13,68 +13,93 @@ import itertools
 # Thus the negative log likelihood is then 
 # - y^{(i)} \log(\hat{y}^{(i)}) + (1 - y^{(i)})\log(1 - \hat{y}^{(i)})
     
-# This is the objective funciton of our logistic classifier.
+# This is the objective function of our logistic classifier.
 def cross_entropy_loss(pred, y):
-        return ( -y * np.log(pred) - (1 - y) * np.log(1 - pred) ).sum()
+    return ( -y * np.log(pred) - (1 - y) * np.log(1 - pred) ).sum()
+
+
+# A numerically stable implementation of the sigmoid function
+# using the "logsumexp" trick
+def sigmoid(z):
+    # Set c to be the elementwise max of 0 and -z
+    c = np.maximum(0, -z)
+    
+    try:
+        return np.exp(-c - np.log(np.exp(-c) + np.exp(-z - c)))
+    except RuntimeWarning:
+            print('Exponential overflow in sigmoid')
 
 
 class LogisticRegressor:
     def __init__(self, 
                  batch_size=1,
-                 learning_rate=0.01,
+                 learning_rate=0.0005,
                  num_epochs=100,
                  regularization_strength=0,
                  seed=None):
         
         self.batch_size = batch_size
-        self.learing_rate=learning_rate
+        self.learning_rate=learning_rate
         self.num_epochs=num_epochs
         self.regularization_strength = regularization_strength
 
         self.rng = np.random.default_rng(seed)
     
     # TODO: implement ADAM
-    def fit(self, X, y, learning_curve=False):
-        # Concatenate 1's for bias terms
-        X = np.concatenate([np.ones(X.shape[0]), X], axis=1)
-
-        # Add one for bias 
-        self.weights = self.rng.random(X.shape[0] + 1)
+    # TODO: Is it possible to get a fully vectorized implementation
+    #       that eliminates the need for the outerloop?
+    # TODO: clip predictions to prevent log(0), clip gradients for numerical stability
+    def fit(self, X, y, learning_curve=False, add_bias=True):
+        if add_bias:
+            # Concatenate 1's for bias terms
+            X = np.concatenate([np.ones((X.shape[0], 1)), X], axis=1)
+        
+        self.weights = self.rng.random((X.shape[1], 1))
 
         if(learning_curve):
-            errors = [self.loss(self.predict(X), y)]
+            errors = []
 
         for epoch_count in range(self.num_epochs):
-            # Permute our rows for a new epoch
-            X_p = self.rng.permute(X)
+            # Permutation to apply to our rows for stochasticity
+            indices = self.rng.permutation(X.shape[0])
 
-            for batch in itertools.batched(X_p, self.batch_size):
-                self.weights = ( 
-                    self.weights -
-                    self.learning_rate * self.loss_gradient(batch, y) 
-                )
+            for batch in itertools.batched(indices, self.batch_size):
+                # Convert to ndarray to trigger advanced indexing
+                ndbatch = np.asarray(batch)
+                # Weight update
+                try:
+                    self.weights -= self.learning_rate * self.loss_gradient(X[ndbatch], y[ndbatch])
+                except TypeError:
+                    print('Prediction error')
+
 
             if(learning_curve):
-                errors.append(self.loss(X, y))
+                try:
+                    errors.append(self.loss(X, y))
+                except RuntimeWarning:
+                    print('Division by 0 in log')
 
         return errors
     
     # Our loss is the L2-regularized cross entropy loss
     def loss(self, X, y):
-        return ( 
-            cross_entropy_loss(self.predict(X), y) +
-                (self.regularization_strength / 2.0) * (
-                        np.linalg.norm(self.weights) ** 2 - self.weights[0] # no bias penalty
-                    )
-        )
+        ce_loss = cross_entropy_loss(self.predict(X), y)
+        # Exclude bias term from regularization penalty
+        reg_penalty = (self.regularization_strength / 2) * np.sum(self.weights[1:] ** 2)
+        return ce_loss + reg_penalty
     
     # The gradient of our loss function
     def loss_gradient(self, X, y):
-        return ( 
-            np.sum(X * (self.predict(X) - y)) + 
-            self.regularization_strength * self.weights
-        )
+        grad = X.T @ (self.predict(X) - y)
+        # Add derivative term for regularization
+        grad[1:] += self.regularization_strength * self.weights[1:]
+        return grad
     
     # prediction of logistic classifer use the sigmoid function
-    def predict(self, x):
-        return 1.0 / (1 - np.exp(-self.weights.transpose() @ x))
+    def predict(self, X):
+        # Concatenate 1 for bias terms
+        # X = np.concatenate([np.ones((X.shape[0], 1)), X], axis=1)
+        return sigmoid(X @ self.weights)
+        # Concatenate 1 for bias terms
+        # X = np.concatenate([np.ones((X.shape[0], 1)), X], axis=1)
+        return sigmoid(X @ self.weights)